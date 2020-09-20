8aeb46007b0459bfc0d1ced4d594e66ba5edf9bb
gzlin7 <gloriazlin@yahoo.com>
2020-09-20 04:15:17 -0400

    rip getting memories

--- src/CampusMap.js
+++ src/CampusMap.js
@@ -1,40 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { GoogleMap, Marker, MarkerClusterer } from "@react-google-maps/api";
 import Pin from "./Pin";
 import { db } from "./services/firebase";
 import pinkBeaver from "./assets/pinkBeaver.png";
 import mapStyles from "./Styles";
+import { setConstantValue } from "typescript";
 
 // Get a reference to the database service
 const database = db.ref();
 const memsRef = database.child('memories');
 
-function getMemories() {
-  var mems = [];
-  memsRef.once("value", function(snapshot) {
-    snapshot.forEach(function(child) {
-      mems.push(child.val());
-    });
-    return mems;
-  });
-}
-
-function getLatLongMemories(lat, long) {
-  var mems = [];
-  memsRef.once("value", function(snapshot) {
-    snapshot.forEach(function(child) {
-      let mem = child.val();
-      if (mem.lat === lat && mem.long === long) {
-        mems.push(mem);
-      }
-    });
-    return mems;
-  });
-}
-}
-
 const CampusMap = () => {
   const initialCoord = { lat: 42.35898, lng: -71.093489 };
+
+  const [markers, setMarkers] = useState([]);
+
+  async function getMemories() {
+    var mems = [];
+    memsRef.once("value", function (snapshot) {
+      snapshot.forEach(function (child) {
+        mems.push(child.val())
+      });
+      console.log("get Memories" + mems)
+    }).then(makeMarkers(mems));
+  }
+
+  function getLatLongMemories(lat, long) {
+    var mems = [];
+    memsRef.once("value", function(snapshot) {
+      snapshot.forEach(function(child) {
+        let mem = child.val();
+        if (mem.lat === lat && mem.long === long) {
+          mems.push(mem);
+        }
+      });
+      return mems;
+    });
+  }
+
+  function makeMarkers(memories) {
+    let markers = [];
+    for (var memory in memories) {
+      let coord = { lat: parseFloat(memory["lat"]), lng: parseFloat(memory["long"]) };
+      markers.push(<Marker position={coord} icon={pinkBeaver} draggable={true} />);
+      console.log("markers" + markers);
+    }
+    setMarkers(markers);
+  }
+
+  // load memories and construct markers
+  useEffect(() => { getMemories();}, [initialCoord]);
 
   return (
     <GoogleMap
@@ -49,7 +64,8 @@ const CampusMap = () => {
       // The latitude and longitude to center the map (always required)
       options={{ styles: mapStyles }}
     >
-      <Marker position={initialCoord} icon={pinkBeaver} draggable={true} />
+      {markers.map((marker, index) => (
+        { marker }))}
     </GoogleMap>
   );
 };
@@ -60,10 +76,10 @@ Map.defaultProps = {
     width: "800px",
   },
   children: null,
-  onLoad: () => {},
-  onDragEndFunc: () => {},
-  onDragStartFunc: () => {},
-  onZoomChangeFunc: () => {},
+  onLoad: () => { },
+  onDragEndFunc: () => { },
+  onDragStartFunc: () => { },
+  onZoomChangeFunc: () => { },
 };
 
 export default CampusMap;
